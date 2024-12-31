39c9ff1ad2c9937c7c2f3d6cd9c3b885135fff0f
Gurdeep <saini.gurdeep1688@gmail.com>
2024-12-31 13:52:24 +0530

    lint issue fixes

--- app/src/main/java/com/example/calsi/MainActivity.kt
+++ app/src/main/java/com/example/calsi/MainActivity.kt
@@ -7,11 +7,10 @@ import android.widget.Button
 import android.widget.TextView
 import java.lang.ArithmeticException
 
-class MainActivity : AppCompatActivity()
-{
+class MainActivity : AppCompatActivity() {
     private var tvinput: TextView? = null
-    var lastNumeric : Boolean = false
-    var lastdot : Boolean = false
+    var lastNumeric: Boolean = false
+    var lastdot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,22 +19,18 @@ class MainActivity : AppCompatActivity()
         tvinput = findViewById(R.id.tvinput)
     }
 
-    fun onDigit(view: View)
-    {
+    fun onDigit(view: View) {
         tvinput?.append((view as Button).text)
         lastNumeric = true
         lastdot = false
     }
 
-    fun clr(view: View)
-    {
+    fun clr(view: View) {
         tvinput?.text = ""
     }
 
-    fun onDecimal(view: View)
-    {
-        if(lastNumeric && !lastdot)
-        {
+    fun onDecimal(view: View) {
+        if (lastNumeric && !lastdot) {
             tvinput?.append(".")
             lastNumeric = false
             lastdot = true
@@ -43,12 +38,10 @@ class MainActivity : AppCompatActivity()
     }
 
 
-    fun onOperator(view: View)
-    {
+    fun onOperator(view: View) {
         tvinput?.text?.let {
 
-            if (lastNumeric && !isOperatorAdded(it.toString()))
-            {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
                 tvinput?.append((view as Button).text)
                 lastNumeric = false
                 lastdot = false
@@ -56,69 +49,55 @@ class MainActivity : AppCompatActivity()
         }
     }
 
-    fun onEqual(view: View)
-    {
-        if (lastNumeric)
-        {
+    fun onEqual(view: View) {
+        if (lastNumeric) {
             var tvValue = tvinput?.text.toString()
 
             var prefix = ""
 
             try {
 
-                if(tvValue.startsWith("-"))
-                {
+                if (tvValue.startsWith("-")) {
                     prefix = "-"
                     tvValue = tvValue.substring(1)
                 }
 
-                if(tvValue.contains("-"))
-                {
+                if (tvValue.contains("-")) {
                     val splitVaalue = tvValue.split("-")
                     var one = splitVaalue[0]
                     var two = splitVaalue[1]
 
-                    if(prefix.isNotEmpty())
-                    {
+                    if (prefix.isNotEmpty()) {
                         one = prefix + one
                     }
                     tvinput?.text = removezeero((one.toDouble() - two.toDouble()).toString())
 
-                }
-                else if(tvValue.contains("+"))
-                {
+                } else if (tvValue.contains("+")) {
                     val splitVaalue = tvValue.split("+")
                     var one = splitVaalue[0]
                     var two = splitVaalue[1]
 
-                    if(prefix.isNotEmpty())
-                    {
+                    if (prefix.isNotEmpty()) {
                         one = prefix + one
                     }
                     tvinput?.text = removezeero((one.toDouble() + two.toDouble()).toString())
 
-                }
-                else if(tvValue.contains("*"))
-                {
+                } else if (tvValue.contains("*")) {
                     val splitVaalue = tvValue.split("*")
                     var one = splitVaalue[0]
                     var two = splitVaalue[1]
 
-                    if(prefix.isNotEmpty())
-                    {
+                    if (prefix.isNotEmpty()) {
                         one = prefix + one
                     }
                     tvinput?.text = removezeero((one.toDouble() * two.toDouble()).toString())
 
-                }
-                else if(tvValue.contains("/"))
-                {
+                } else if (tvValue.contains("/")) {
                     val splitVaalue = tvValue.split("/")
                     var one = splitVaalue[0]
                     var two = splitVaalue[1]
 
-                    if(prefix.isNotEmpty())
-                    {
+                    if (prefix.isNotEmpty()) {
                         one = prefix + one
                     }
                     tvinput?.text = removezeero((one.toDouble() / two.toDouble()).toString())
@@ -130,23 +109,19 @@ class MainActivity : AppCompatActivity()
         }
     }
 
-    private fun removezeero(result : String) : String
-    {
+    private fun removezeero(result: String): String {
         var value = result
-        if(result.contains(".0"))
-        value = result.substring(0, result.length - 2)
+        if (result.contains(".0"))
+            value = result.substring(0, result.length - 2)
 
         return value
     }
 
-    private fun isOperatorAdded(value : String) : Boolean {
+    private fun isOperatorAdded(value: String): Boolean {
 
-        return if(value.startsWith("-"))
-        {
+        return if (value.startsWith("-")) {
             false
-        }
-        else
-        {
+        } else {
             value.contains("+")
                     || value.contains("*")
                     || value.contains("/")
